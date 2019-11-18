a0bb9430ffc71219f1dc17595cbc25d5e2495872
yuriatgoogle <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2019-11-18 11:10:45 -0800

    added debugger demo

---
+++ debugger-demo/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const projectID = 'stack-doctor';
+const serviceName = "debugger-demo";
+const serviceVersion = "1.0"
+
+require('@google-cloud/debug-agent').start({
+    projectId: projectID,
+    serviceContext: {
+      service: serviceName,
+      version: serviceVersion
+    }
+  });
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+app.get('/', (req, res) => {
+    console.log('request made');
+    randomInt = getRandomInt(10);
+    if (randomInt > 5) {
+        console.log("an error was encountered")
+        res.status(500).send("there was an error!")
+    }
+    res.send('request made');
+})
+
+app.listen(8080, () => console.log(`Example app listening on port 8080!`))
