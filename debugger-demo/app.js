ca6e8bb2ed6fa484c6d9aa4df17346214bffa4e9
yuriatgoogle <ygrinshteyn@ygrinshteyn-macbookpro1.roam.corp.google.com>
2019-11-21 14:20:03 -0800

    updating service version

--- debugger-demo/app.js
+++ debugger-demo/app.js
@@ -3,7 +3,7 @@ const app = express();
 
 const projectID = 'stack-doctor';
 const serviceName = "debugger-demo";
-const serviceVersion = "1.0"
+const serviceVersion = "1.1"
 
 require('@google-cloud/debug-agent').start({
     projectId: projectID,
