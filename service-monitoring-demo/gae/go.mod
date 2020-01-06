31f9618f89802bbcb05ecfdd410267b8966fba7e
yuriatgoogle <ygrinshteyn@google.com>
2020-01-06 10:00:07 -0800

    adding service monitoring demo

---
+++ service-monitoring-demo/gae/go.mod
@@ -0,0 +1,3 @@
+module githu.com/yuriatgoogle/stack-doctor/service-monitoring-demo
+
+go 1.13
---
+++ service-monitoring-demo/gae/helloworld.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World!")
+	})
+
+	http.ListenAndServe(":8080", r)
+}
