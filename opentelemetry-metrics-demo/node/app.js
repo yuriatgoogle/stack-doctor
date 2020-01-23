6de975ab432fc7db1e146a57dbf3c427dfdef25f
Yuri Grinshteyn <ygrinshteyn@google.com>
2020-01-23 12:09:58 -0800

    golden signals metrics working in node

--- opentelemetry-metrics-demo/node/app.js
+++ opentelemetry-metrics-demo/node/app.js
@@ -1,34 +1,70 @@
 "use strict";
 
+// imports
+const express = require ('express')
 const { MeterRegistry } = require('@opentelemetry/metrics');
 const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
 
-const meter = new MeterRegistry().getMeter('example-prometheus');
+function sleep (n) {
+    Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
+}
 
+// set up prometheus 
+const app = express()
+const meter = new MeterRegistry().getMeter('example-prometheus');
 const exporter = new PrometheusExporter(
   {
     startServer: true,
-    port: 8080
+    port: 8081
   },
   () => {
     console.log("prometheus scrape endpoint: http://localhost:8080/metrics");
   }
 );
-
 meter.addExporter(exporter);
 
-
-const nonMonotonicGauge = meter.createGauge("non_monotonic_gauge", {
-  monotonic: false,
+// define metrics with description and labels
+const requestCount = meter.createCounter("request_count", {
+  monotonic: true,
   labelKeys: ["metricOrigin"],
-  description: "Example of a non-monotonic gauge"
+  description: "Counts total number of requests"
 });
+const errorCount = meter.createCounter("error_count", {
+    monotonic: true,
+    labelKeys: ["metricOrigin"],
+    description: "Counts total number of errors"
+});
+const responseLatency = meter.createGauge("response_latency", {
+    monotonic: false,
+    labelKeys: ["metricOrigin"],
+    description: "Records latency of response"
+});
+const labels = meter.labels({ metricOrigin: process.env.ENV});
 
-let metricValue = 0;
-setInterval(() => {
-  const labels = meter.labels({ metricOrigin: process.env.ENV});
-  metricValue = Math.floor(Math.random() * 100);
-  nonMonotonicGauge
-    .bind(labels)
-    .set(metricValue);
-}, 1000);
+
+// set metric values on request
+
+app.get('/', (req, res) => {
+    // start latency timer
+    const requestReceived = new Date().getTime();
+    console.log('request made');
+    // increment total requests counter
+    requestCount.bind(labels).add(1);
+    // return an error 1% of the time
+    if ((Math.floor(Math.random() * 100)) > 50) {
+        // increment error counter
+        errorCount.bind(labels).add(1);
+        // return error code
+        res.status(500).send("error!")
+    } 
+    else {
+        // delay for a bit
+        sleep(Math.floor(Math.random()*10000));
+        // record response latency
+        const measuredLatency = new Date().getTime() - requestReceived;
+        responseLatency.bind(labels).set(measuredLatency)
+        res.status(200).send("success in " + measuredLatency + " ms")
+    }
+})
+
+app.listen(8080, () => console.log(`Example app listening on port 8080!`))
