a92d0f30571f287f08f9a55551100f2b82731b6a
Yuri Grinshteyn <ygrinshteyn@google.com>
2020-08-31 10:37:17 -0700

    Added second SLO

--- opentelemetry-metrics-demo/node/custom_service.tf
+++ opentelemetry-metrics-demo/node/custom_service.tf
@@ -9,7 +9,7 @@ resource "google_monitoring_custom_service" "terraform-service" {
   display_name = "Service Created by Terraform"
 }
 
-resource "google_monitoring_slo" "request_based_slo" {
+resource "google_monitoring_slo" "window_based_slo" {
   service = google_monitoring_custom_service.terraform-service.service_id
   slo_id = "terraform-slo"
   display_name = "99% of 10-min windows in rolling day have mean latency under 8s"
@@ -31,4 +31,31 @@ resource "google_monitoring_slo" "request_based_slo" {
       }
     }
   }
+}
+
+resource "google_monitoring_slo" "request_based_slo" {
+  # the basics
+  service = google_monitoring_custom_service.terraform-service.service_id
+  slo_id = "request-slo"
+  display_name = "99% of requests are successful in a rolling day"
+
+  # the SLI
+  request_based_sli {
+    good_total_ratio {
+      total_service_filter = join(" AND ", [
+        "metric.type=\"external.googleapis.com/prometheus/request_count\"",
+        "resource.type=\"k8s_container\"",
+        "resource.label.\"container_name\"=\"opentelemetry-server\""
+      ])
+      bad_service_filter = join(" AND ", [
+        "metric.type=\"external.googleapis.com/prometheus/error_count\"",
+        "resource.type=\"k8s_container\"",
+        "resource.label.\"container_name\"=\"opentelemetry-server\"" 
+      ])
+    }
+  }
+
+  # the goal
+  goal = 0.99
+  rolling_period_days = 1
 }
