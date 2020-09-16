c260cad7650c1a42b541aa3ce6d390b9a5edf301
Aaron Abbott <aaronabbott@google.com>
2020-09-16 15:57:11 +0000

    use gcp resource detector
    Signed-off-by: Aaron Abbott <aaronabbott@google.com>

--- slo-alerting-demo/app.js
+++ slo-alerting-demo/app.js
@@ -5,51 +5,59 @@ const express = require ('express');
 const app = express();
 const { MeterProvider } = require('@opentelemetry/metrics');
 const { MetricExporter } = require('@google-cloud/opentelemetry-cloud-monitoring-exporter');
+const {gcpDetector} = require('@opentelemetry/resource-detector-gcp');
 
 function sleep (n) {
   Atomics.wait(new Int32Array(new SharedArrayBuffer(4)), 0, 0, n);
 }
 
-const exporter = new MetricExporter();
-const ERROR_RATE = process.env.ERROR_RATE;
+async function main() {
+  const exporter = new MetricExporter();
+  const ERROR_RATE = process.env.ERROR_RATE;
+  const resource = await gcpDetector.detect();
 
-// Register the exporter
-const meter = new MeterProvider({
-  exporter,
-  interval: 60000,
-}).getMeter('example-meter');
+  // Register the exporter
+  const meter = new MeterProvider({
+    exporter,
+    interval: 60000,
+    resource,
+  }).getMeter('example-meter');
 
-// define metrics 
-const requestCount = meter.createCounter("request_count_sli", {
-  monotonic: true,
-  labelKeys: ["metricOrigin"],
-  description: "Counts total number of requests"
-});
-const errorCount = meter.createCounter("error_count_sli", {
+  // define metrics 
+  const requestCount = meter.createCounter("request_count_sli", {
     monotonic: true,
     labelKeys: ["metricOrigin"],
-    description: "Counts total number of errors"
-});
-// const labels = meter.labels({ metricOrigin: process.env.ENV});
+    description: "Counts total number of requests"
+  });
+  const errorCount = meter.createCounter("error_count_sli", {
+      monotonic: true,
+      labelKeys: ["metricOrigin"],
+      description: "Counts total number of errors"
+  });
+  // const labels = meter.labels({ metricOrigin: process.env.ENV});
 
-// set metric values on request
-app.get('/', (req, res) => {
-    // start latency timer
-    console.log('request made');
-    // increment total requests counter
-    requestCount.add(1);
-    // return an error based on ERROR_RATE
-    if ((Math.floor(Math.random() * 100)) <= ERROR_RATE) {
-        // increment error counter
-        errorCount.add(1);
-        // return error code
-        res.status(500).send("error!")
-    } 
-    // record latency and respond right away
-    else {
-      sleep(Math.floor(Math.random()*1000));
-      res.status(200).send("success!")
-    }
-})
+  // set metric values on request
+  app.get('/', (req, res) => {
+      // start latency timer
+      console.log('request made');
+      // increment total requests counter
+      requestCount.add(1);
+      // return an error based on ERROR_RATE
+      if ((Math.floor(Math.random() * 100)) <= ERROR_RATE) {
+          // increment error counter
+          errorCount.add(1);
+          // return error code
+          res.status(500).send("error!")
+      } 
+      // record latency and respond right away
+      else {
+        sleep(Math.floor(Math.random()*1000));
+        res.status(200).send("success!")
+      }
+  })
 
-app.listen(8080, () => console.log(`Example app listening on port 8080!`))
+  app.listen(8080, () => console.log(`Example app listening on port 8080!`))
+}
+
+main();
+
