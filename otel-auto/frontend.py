5451a18dcbd92ef69226689569b3e6d761c96dea
Yuri Grinshteyn <ygrinshteyn@google.com>
2022-03-21 15:47:51 -0700

    changed frontend to take env variable for backend

--- otel-auto/frontend.py
+++ otel-auto/frontend.py
@@ -1,13 +1,16 @@
 from flask import Flask
 import requests
 import time
+import os
+
+backend_addr = os.getenv('BACKEND')
 
 app = Flask(__name__)
 
 @ app.route('/')
 def index():
     start = time.time()
-    r = requests.get('https://www.google.com')
+    r = requests.get(backend_addr)
     latency = time.time() - start
     return 'Response from backend: ' + str(r.status_code) + ' in ' + str(round(latency, 3) * 1000) + ' ms'
 
