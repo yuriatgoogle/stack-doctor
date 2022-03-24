f263ce31c290cb59bf109bc28cc2885905e8ff2b
Yuri Grinshteyn <ygrinshteyn@google.com>
2022-03-24 15:18:46 -0700

    make otel go?

--- otel-auto/auto/frontend/frontend.py
+++ otel-auto/auto/frontend/frontend.py
@@ -3,6 +3,32 @@ import requests
 import time
 import os
 
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+trace.set_tracer_provider(
+TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "my-helloworld-service"})
+    )
+)
+tracer = trace.get_tracer(__name__)
+
+# create a JaegerExporter
+jaeger_exporter = JaegerExporter(
+    # configure agent
+    agent_host_name='localhost',
+    agent_port=6831,
+)
+
+# Create a BatchSpanProcessor and add the exporter to it
+span_processor = BatchSpanProcessor(jaeger_exporter)
+
+# add to the tracer
+trace.get_tracer_provider().add_span_processor(span_processor)
+
 backend_addr = os.getenv('BACKEND')
 
 app = Flask(__name__)
