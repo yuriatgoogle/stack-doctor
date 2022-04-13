1d180e3ef52caed9b7d763c22f70a9f8cdcd5001
Yuri Grinshteyn <ygrinshteyn@google.com>
2022-04-13 09:20:07 -0700

    otel metrics run locally

--- opentelemetry-metrics-demo/python/app.py
+++ opentelemetry-metrics-demo/python/app.py
@@ -1,58 +1,44 @@
-from flask import Flask, request
-import requests
+from flask import Flask
 import time
 import os
 from random import randint
 from time import sleep
 
-from opentelemetry import metrics
-from opentelemetry.exporter.cloud_monitoring import (
-    CloudMonitoringMetricsExporter,
-)
-from opentelemetry.sdk.metrics import MeterProvider
 
-metrics.set_meter_provider(MeterProvider())
-meter = metrics.get_meter(__name__)
-metrics.get_meter_provider().start_pipeline(
-    meter, CloudMonitoringMetricsExporter(), 5
-)
+from opentelemetry._metrics import get_meter_provider, set_meter_provider
+from opentelemetry.exporter.otlp.proto.grpc._metric_exporter import OTLPMetricExporter
+from opentelemetry.sdk._metrics import MeterProvider
+from opentelemetry.sdk._metrics.export import PeriodicExportingMetricReader
 
-requests_counter = meter.create_counter(
-    name="otel_total_requests",
-    description="total requests",
-    unit="1",
-    value_type=int,
+meter = get_meter_provider().get_meter("getting-started")
+metric_exporter = OTLPMetricExporter(
+    # endpoint:="localhost:55678",
+    # credentials=ChannelCredentials(credentials),
+    # headers=(("metadata", "metadata")),
 )
+reader = PeriodicExportingMetricReader(metric_exporter)
+provider = MeterProvider(metric_readers=[reader])
+set_meter_provider(provider)
 
-errors_counter = meter.create_counter(
-    name="otel_failed_requests",
-    description="failed requests",
-    unit="1",
-    value_type=int,
-)
-
-request_latency = meter.create_valuerecorder(
-    name="otel_request_latency",
-    description="request latency",
-    unit='ms',
-    value_type=float
-)
+requests_counter = meter.create_counter("otel_total_requests")
+errors_counter = meter.create_counter("otel_failed_requests")
+# request_latency = meter.create_valuerecorder("otel_request_latency")
 
 labels = {}
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    requests_counter.add(1, labels=labels)
+    requests_counter.add(1)
     start = time.time()
     sleep(randint(1,1000)/1000)
     latency = time.time() - start
     if randint(1,100) > 95:
         # fail 5 % of the time
-        errors_counter.add(1, labels=labels)
-        request_latency.record(latency)
+        errors_counter.add(1)
+        # request_latency.record(latency)
         return 'Processing failed!', 500
-    request_latency.record(latency, labels=labels)
+    # request_latency.record(latency, labels=labels)
     return 'returned in ' + str(round(latency, 3) * 1000) + ' ms', 200
     
 app.run(host='0.0.0.0', port=8080)
